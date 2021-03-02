c98466c768533a3e5b50fc72f19fa6ec0b002b8d
Unai <unaiza@edu.umea.se>
2021-03-02 11:29:53 +0100

    Test 3

--- assignment_1/js/WildArch.js
+++ assignment_1/js/WildArch.js
@@ -2,9 +2,23 @@
     Do everything you want in functions because you do not want to define global variables.
 */
 
-function bingusGunction(){
-    
+function example() {
+    let nameElement = document.getElementById("WildArch");
+    let objectEl = document.createElement("h3");
+    objectEl.textContent = "Joe mama :)";
+    nameElement.appendChild(objectEl);
 }
 
 example();
+
+
+function triangleFunction();{
+    document.getElementById("demo").innerHTML = "Hello World";
+
+
+
+
+};
+
+
 
