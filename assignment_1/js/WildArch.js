7ae2c7131567bc327211d9f9b93294f9745669e2
Unai <unaiza@edu.umea.se>
2021-03-02 10:35:13 +0100

    Test 1

--- assignment_1/js/WildArch.js
+++ assignment_1/js/WildArch.js
@@ -2,11 +2,8 @@
     Do everything you want in functions because you do not want to define global variables.
 */
 
-function example() {
-    let nameElement = document.getElementById("WildArch");
-    let objectEl = document.createElement("h3");
-    objectEl.textContent = "Example :)";
-    nameElement.appendChild(objectEl);
+function bingusGunction(){
+    
 }
 
 example();
