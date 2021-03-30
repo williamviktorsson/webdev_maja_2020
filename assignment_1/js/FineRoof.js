cab90def79cd2663fee6dc9f09fe2b029c3f5f5c
ryaalm <sofalm@edu.umea.se>
2021-03-30 14:27:17 +0200

    did i fix? who knows

--- assignment_1/js/FineRoof.js
+++ assignment_1/js/FineRoof.js
@@ -12,6 +12,7 @@ function example() {
 }
 
 function circle(){
+    let FineRoofCanvas = document.getElementById("FineRoof");
     let circleObj = document.createElement("div");
     circleObj.className = "circle";
     circleObj.style.backgroundColor = "rgb(0,128,128)";
@@ -21,6 +22,7 @@ function circle(){
 }
 
 function sweetCaroline(){
+    let FineRoofCanvas = document.getElementById("FineRoof");
     let sweetImage = document.createElement("img");
     sweetImage.src = "https://img.youtube.com/vi/2w-_Vtttrfc/0.jpg";
     sweetImage.alt = "An album photo of Neil Diamond"
@@ -39,6 +41,7 @@ function sweetCaroline(){
 }
 
 function slideshowFunc(){
+    let FineRoofCanvas = document.getElementById("FineRoof");
     let imgList = ["https://img.itch.zone/aW1nLzIzODcxNzguZ2lm/original/U50cJ3.gif", "https://i.pinimg.com/originals/a3/a3/8b/a3a38b2b44db8ac3dee2231f72f46b89.gif"
     , "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b1b1d7fa-6f73-4d3d-8057-9a7c68838ecd/daa0oa2-3252452f-29da-46ea-bffe-2b7261a99cff.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYjFiMWQ3ZmEtNmY3My00ZDNkLTgwNTctOWE3YzY4ODM4ZWNkXC9kYWEwb2EyLTMyNTI0NTJmLTI5ZGEtNDZlYS1iZmZlLTJiNzI2MWE5OWNmZi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.jw3Y5NIQFznC07mDwyoEUhXAQQ_as-cTwimaqDveb7Q"]
     let slideshow = document.createElement("img");
