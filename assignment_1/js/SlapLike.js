5459a0d5bc2ec547c304397c0fe107f6529b67db
elthed02 <elthed02@edu.umea.se>
2021-03-30 13:58:39 +0200

    Elton

--- assignment_1/js/SlapLike.js
+++ assignment_1/js/SlapLike.js
@@ -15,6 +15,8 @@ function example() {
     let img2 = document.createElement("img")
     img1.src = "https://www.kasandbox.org/programming-images/avatars/old-spice-man.png"
     img2.src = "https://www.kasandbox.org/programming-images/avatars/marcimus.png"
+    img1.className = "jakt"
+    img2.className = "jakt"
     
     let cirkel = document.createElement("p")
     cirkel.id = "cirkel"
@@ -55,20 +57,40 @@ function example() {
 
         let button1 = document.createElement("button")
         button1.className = "quizButton"
-        button1.textContent = "Hej"
-        nameElement.appendChild(button1)
+        button1.textContent = "Jacob är bäst"
+        
+        box.appendChild(button1)
+        button1.style.left = "30px"
+        
 
         let button2 = document.createElement("button")
         button2.className = "quizButton"
-        button2.textContent = "Hej"
-        nameElement.appendChild(button2)
-        button2.style.left = "550px"
+        button2.textContent = "Isak är bäst"
+        button2.style.left = "125px"
+        box.appendChild(button2)
+        
 
         let button3 = document.createElement("button")
         button3.className = "quizButton"
-        button3.textContent = "Hej"
-        nameElement.appendChild(button3)
-        button3.style.left = "600px"
+        button3.textContent = "Axel är bäst"
+        button3.style.left = "220px"
+        box.appendChild(button3)
+
+        let jacob = document.createElement("img")
+        jacob.src = "https://www.ufc.se/wp-content/uploads/2020/09/akad_jacob_berglund_200901_xl.jpg"
+        jacob.id = "jacob"
+        box.appendChild(jacob)
+
+        let axel = document.createElement("img")
+        axel.src = "https://media.discordapp.net/attachments/762977870049443861/826422058435084298/image0.jpg?width=283&height=612"
+        axel.id = "axel"
+        box.appendChild(axel)
+
+        let isak = document.createElement("img")
+        isak.src = "https://media.discordapp.net/attachments/762977870049443861/826423295662096394/image0.jpg?width=458&height=612"
+        isak.id = "isak"
+        box.appendChild(isak)
+        
     }
 
    
