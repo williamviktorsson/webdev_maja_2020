3aab70acc632e69052157b89129d03025a706b19
Filip <filsva@edu.umea.se>
2021-03-30 12:31:15 +0200

    huh

--- assignment_1/js/TileCell.js
+++ assignment_1/js/TileCell.js
@@ -3,14 +3,24 @@
 */
 
 function example() {
+    //texten som säger "oändlig smärta"
     let nameElement = document.getElementById("TileCell");
     let objectEl = document.createElement("h3");
     objectEl.textContent = "Oändlig smärta";
     nameElement.appendChild(objectEl);
 
+    //the geometric object
     let squareEl = document.createElement("div");
     squareEl.className = 'Square';
     nameElement.appendChild(squareEl);
+
+    //the colorchanging button
+    let butEl = document.createElement("button")
+    butEl.textContent = "do a switcheroo"
+    let clicked = function(){
+        squareEl.style.backgroundColor = 'green';
+    }
+
 }
 
 example();
