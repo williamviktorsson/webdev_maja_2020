df349038ccb25bcaba28e8448becb1a105f03b88
miltonnm <milrad@edu.umea.se>
2021-03-30 10:31:11 +0200

    sheeeesh

--- assignment_1/js/PourSale.js
+++ assignment_1/js/PourSale.js
@@ -7,7 +7,54 @@ function example() {
     let objectEl = document.createElement("h3");
     objectEl.textContent = "Example :)";
     nameElement.appendChild(objectEl);
+
+    let square = document.createElement("div");
+
+    let spaceboi = document.createElement("div");
+    spaceboi.classList.add("spaceboi");
+    nameElement.appendChild(spaceboi);
+
+    let coolCircle = document.createElement("div");
+    coolCircle.classList.add("coolCircle");
+    nameElement.appendChild(coolCircle);
+
+    let knapp = document.createElement("button");
+    knapp.className = "square";
+    nameElement.appendChild(knapp);
+
+    var onButtonClick = function() {
+        spaceboi.className = "spaceboi2" 
+    };
+
+    knapp.addEventListener("click", onButtonClick);
+
+
 }
+
+var slideShow = function(container) {
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
+    
+    // Handle going to to the next slide
+    var nextSlide = function() {
+        for (var i = 0; i < this.images.length; i++) {
+            this.images[i].style.display = "none";
+        }
+        this.images[this.curImage].style.display = "block";
+        this.curImage++;
+        if (this.curImage >= this.images.length) {
+            this.curImage = 0;
+        }
+        window.setTimeout(nextSlide.bind(this), 1000);
+    };
+    
+    nextSlide.call(this);
+};
+slideShow(document.getElementById("slideshow"));
 
 example();
 
