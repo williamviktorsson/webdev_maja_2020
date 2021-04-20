fa0e51083db2d3fc992bda7ddc385fa13875d6a5
kirstr <kirstr@edu.umea.se>
2021-04-20 10:00:16 +0200

    Circle

--- assignment_1/js/CodeTool.js
+++ assignment_1/js/CodeTool.js
@@ -3,13 +3,11 @@ function example() {
     //Min namnen
     let nameElement = document.getElementById("CodeTool");
     let objectEl = document.createElement("h3");
-    objectEl.textContent = "Amogus";
     nameElement.appendChild(objectEl);
 
     //Min fyrkanten
-    let squareEl = dokument.createElement("div")
-    squareEl.textContent = "KirnisBox";
-    nameElement.appendChild(squareEl);
+    let circle = dokument.createElement("div")
+    circle.className = "KirnisCircle";
 
 }
 
