70d3147a9b572d609c62c0a2247f9270c7ab5f6e
Filip <filsva@edu.umea.se>
2021-04-20 14:47:37 +0200

    huh

--- assignment_1/js/TileCell.js
+++ assignment_1/js/TileCell.js
@@ -9,28 +9,68 @@ function example() {
     objectEl.textContent = "Oändlig smärta";
     nameElement.appendChild(objectEl);
 
-    //the geometric object
+    //the cringe
     var joeMama = document.createElement("div");
-    joeMama.className = 'RedBox';
+    joeMama.className = "RedTri";
     joeMama.id = 'squareID';
     nameElement.appendChild(joeMama);
 
     //the colorchanging button
-    let butEl = document.createElement("button");
-    butEl.textContent = "FORTNITE EPIC";
-    nameElement.appendChild(butEl);
+    let jonasBros = document.createElement("button");
+    jonasBros.textContent = "tomrum";
+    nameElement.appendChild(jonasBros);
 
+    //the cool cringe
+    var deezNuts = document.createElement("div");
+    deezNuts.className = "Box"
+    nameElement.appendChild(deezNuts);
+    
+ 
     let clicked = function(){
-        //let joeMama = document.getElementById('squareID');
-        if(joeMama.className = 'RedBox'){
-            joeMama.className = 'PinkBox';
-        }
-        
+        let vadFan = document.getElementById('squareID');
+        vadFan.className = "PinkTri";   
     }
-    butEl.addEventListener("fortniet", clicked);
-    
-    
+
+    jonasBros.addEventListener("click", clicked);
+
+    //slideshow list of pics
+    let goldScar = document.createElement('div');
+    goldScar.id = "slideshow"
+    let list = ['http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/flushed-face.png', 'https://assets.icanet.se/t_product_large_v1,f_auto/5449000149343.jpg', 'https://lekmer.se/images/367944/full.png']
+    for (let i = 0; i < list.length ;i++){
+        let fortnite = document.createElement('img');
+        fortnite.src = list[i];
+        fortnite.alt = "pictures representing pain";
+        goldScar.appendChild(fortnite)
+    }
+    nameElement.appendChild(goldScar);
 }
 
-example();
+var slide = function(container) {
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
 
+    // Handle going to to the next slide
+    var nextSlide = function() {
+        for (var i = 0; i < this.images.length; i++) {
+            this.images[i].style.display = "none";
+        }
+        this.images[this.curImage].style.display = "block";
+        this.curImage++;
+        if (this.curImage >= this.images.length) {
+            this.curImage = 0;
+        }
+        window.setTimeout(nextSlide.bind(this), 1000);
+    };
+
+    nextSlide.call(this);
+};
+
+example();
+slideShow(document.getElementById("slideshow"));
+
+ 
