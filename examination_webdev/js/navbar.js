1e0165f5531031c19c4494bb7681dc3af122a41e
William Viktorsson <william@viktorsson.com>
2021-04-20 16:56:18 +0200

    navigation source code changed to work better

--- examination_webdev/js/navbar.js
+++ examination_webdev/js/navbar.js
@@ -12,7 +12,7 @@ function addNavBar() {
     };
 
     let navbar = document.getElementById("myNavbar");
-    
+
     for (let key in navigation) {
         let value = navigation[key];
         let element = document.createElement("a");
@@ -22,13 +22,13 @@ function addNavBar() {
     }
 
     setCorrectNavBarItemAsActive();
-
 }
 
 function setCorrectNavBarItemAsActive() {
     let array = document.getElementById("myNavbar").children;
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        element.className += (element.hasAttribute("href") && element.href.includes(location.pathname)) ? "active" : ""
+        element.className += (element.hasAttribute("href") && element.href.includes(location.pathname.includes(".html") ? location.pathname : "index.html")) ? " active" : ""
+
     }
 }
