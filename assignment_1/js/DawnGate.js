1a956085dd46d24ed6bf70be45c29da7ff0e449b
linus <linman02@edu.umea.se>
2021-04-26 23:53:33 +0200

    finished assignment

--- assignment_1/js/DawnGate.js
+++ assignment_1/js/DawnGate.js
@@ -7,7 +7,22 @@ function example() {
     let objectEl = document.createElement("h3");
     objectEl.textContent = "Example :)";
     nameElement.appendChild(objectEl);
-}
+
+    let cubeEl = document.createElement("div");
+    cubeEl.className = "cubeEl";
+    cubeEl.style.height = "70px";
+    cubeEl.style.width = "70px";
+    cubeEl.style.backgroundColor = "red";
+    nameElement.appendChild(cubeEl);
+
+    let button = document.createElement("button");
+    button.textContent = "click";
+    let buttonClick = function() {
+        cubeEl.style.backgroundColor = "green";
+    };
+    button.addEventListener("click", buttonClick);
+    nameElement.appendChild(button);
+
+} 
 
 example();
-
