3ea4c746026eef47b007bf844bea7609b8f7971f
albsil02 <albsil@UKE-PF1S31XX.localdomain>
2021-04-27 10:18:04 +0200

    finished assignment

--- assignment_1/js/RootBend.js
+++ assignment_1/js/RootBend.js
@@ -7,13 +7,19 @@ function example() {
     let objectEl = document.createElement("h3");
     objectEl.textContent = "Bamses bananer";
     nameElement.appendChild(objectEl);
+
+    let kanpp = document.createElement("Kanpp")
+    kanpp.textContent = "Byt färg";
+    let buttonClick= function(){
+        h3.color= "red";
+    }
+
 }
 
     example()
 
-    function buttons(){
-        let name=document.getElementById("RootBend");
-        let buttonClick = document.createElement("button");
+    
+        
         
 
-    }
+    
