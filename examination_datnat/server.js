bf0135f4e32cce5d42852e6de127e538234e6896
William Viktorsson <william@viktorsson.com>
2021-06-07 23:36:55 +0200

    added exam code

---
+++ examination_datnat/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const os = require('os');
+
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end(os.hostname());
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
