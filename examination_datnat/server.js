776acad7bbc23751de883254be32caa62fc8beb4
williamv <williamv@cs.umu.se>
2021-06-08 11:03:00 +0200

    server print

--- examination_datnat/server.js
+++ examination_datnat/server.js
@@ -6,6 +6,6 @@ const requestListener = function (req, res) {
   res.writeHead(200);
   res.end(os.hostname());
 }
-
+console.log("Server Started!");
 const server = http.createServer(requestListener);
 server.listen(8080);
